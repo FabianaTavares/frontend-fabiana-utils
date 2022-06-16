5563e801efbc6c2fd25c8bfcbec7f956038196bb
Fabiana Tavares <fabi.tavares1@gmail.com>
2022-06-16 15:45:47 -0300

    feat: criação das pastas de assets e um mock referente a status de anexos, model, interface e enum

---
+++ src/assets/mocks/mock-status-anexos.ts
@@ -0,0 +1,11 @@
+import { StatusAnexos } from "src/app/shared/models/interface/status-anexos.model";
+
+
+export const STATUS_ANEXOS: StatusAnexos[] = [
+  { id: 12, codigo: 'RECEBIDO', descricao: 'Recebido' },
+  { id: 13, codigo: 'REPROVADO', descricao: 'Reprovado' },
+  { id: 14, codigo: 'EM ANALISE', descricao: 'Em Analise' },
+  { id: 15, codigo: 'PENDENTE', descricao: 'Pendente' },
+  { id: 16, codigo: 'CANCELADO', descricao: 'Cancelado' },
+  { id: 17, codigo: 'APROVADO', descricao: 'Aprovado' },
+];
