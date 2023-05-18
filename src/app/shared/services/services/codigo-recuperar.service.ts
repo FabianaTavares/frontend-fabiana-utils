2e97ac76e44f24cc4dbaaa2925f30cfc0bc6624e
Fabiana Tavares <fabi.tavares1@gmail.com>
2023-05-18 12:19:34 -0300

    schematics

--- src/app/shared/services/services/codigo-recuperar.service.ts
+++ src/app/shared/services/services/codigo-recuperar.service.ts
@@ -15,25 +15,25 @@ export class CodigoRecuperarService {
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {
-      this.router.events.pipe(
-        filter(event => event instanceof NavigationEnd)
-      )
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    )
       .subscribe(() => {
         this.codigoRecuperadoAtual = this.recuperaParametroId(this.activatedRoute);
         this.setCodigoUsuario(this.codigoRecuperadoAtual);
       })
-    }
+  }
 
   getCodigoUsuario(): string {
     return this.codigoRecuperadoAtual ? this.codigoRecuperadoAtual : this.recuperaParametroId(this.activatedRoute);
   }
 
-  private setCodigoUsuario(CodigoUsuario: string){
+  private setCodigoUsuario(CodigoUsuario: string) {
     this.codigoRecuperar$.next(CodigoUsuario);
   }
 
-  private recuperaParametroId(actRouter: ActivatedRoute){
-    if(actRouter && actRouter.children && actRouter.children.length){
+  private recuperaParametroId(actRouter: ActivatedRoute) {
+    if (actRouter && actRouter.children && actRouter.children.length) {
       const actRouterAtual = actRouter.children.find(router => router.snapshot.params['codigo']);
       return actRouterAtual != null ? actRouterAtual.snapshot.params['codigo'] : this.recuperaParametroId(actRouter.children[0]);
     }
